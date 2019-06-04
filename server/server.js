632f517fbaf470bd417e400f9b69c422c89c79c7
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-06-04 10:20:02 -0700

    Add server file with express in react

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(3030, () => {
+  console.log("Server is on port 3030.")
+})
+
+
+
+/*
+SCHEMAS:
+  USERS:
+  PRODUCTS:
+
+
+*/
