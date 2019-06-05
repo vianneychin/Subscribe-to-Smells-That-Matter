4a5da46d0da47e3e6c5a3e5247c4258992753dd6
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-06-05 10:45:38 -0700

    remove styled from shopping cart

--- src/Components/ShoppingCart/StyledShoppingCart.js
+++ src/Components/ShoppingCart/StyledShoppingCart.js
@@ -1,5 +1,53 @@
 import styled from 'styled-components'
 
 export const StyledShoppingCart = styled.div`
-  
+  /* height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  > main {
+      display: flex;
+      flex-direction: column;
+      background-color: red;
+      width: 1200px;
+      height: 100vh;
+    > section:nth-of-type(1) {
+        display: flex;
+        text-align: center;
+        flex-direction: column;
+        justify-content: center;;
+        height: 20vh;
+        background-color: grey;
+        > h2 {
+            background-color: beige;
+        }
+        > h3 {
+            background-color: bisque;
+        }
+    }
+    > section:nth-of-type(2) {
+        display: flex;
+        > aside {
+          background-color: yellow;
+        }
+      > div:nth-of-type(1) {
+          width: 500px;
+          height: 500px;
+          margin-right: 0;
+      }
+    }
+  }
+  @media (max-width: 640px) {
+    background-color: blue;
+    > section:nth-of-type(1) {
+      justify-content: center;
+    }
+    img {
+      height: 300px;
+      width: 300px;
+    }
+  } */
+  } */
 `
